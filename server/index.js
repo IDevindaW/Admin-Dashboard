3e66dfa92eafedf9928fbfb09d46d965c047590b
Isuru <isurdevinda1980716u@gmail.com>
2025-04-11 04:51:06 +0530

    product page updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import {dataUser} from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import {dataUser,dataProduct,dataProductStat} from "./data/index.js";
 
 //config
 dotenv.config();
@@ -47,8 +49,8 @@ mongoose
     /* ONLY ADD DATA ONE TIME */
     // AffiliateStat.insertMany(dataAffiliateStat);
     // OverallStat.insertMany(dataOverallStat);
-    // Product.insertMany(dataProduct);
-    // ProductStat.insertMany(dataProductStat);
+    //Product.insertMany(dataProduct);
+    //ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
     //User.insertMany(dataUser);
   })
